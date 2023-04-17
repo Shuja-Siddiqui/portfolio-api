a3a23d786300165c02bab310d2f2ed6f77d92dec
Shuja-Siddiqui <shuja0094@gmail.com>
2023-04-17 15:59:31 +0500

    sevices

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const dev = require("./dev_information");
+const service = require("./services");
 
 router.use("/dev_information", dev);
+router.use("/services", service);
 
 module.exports = router;
