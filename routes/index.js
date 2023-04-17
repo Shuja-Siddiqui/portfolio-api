a16945688cece6faf3375a0ac93225333e524c6d
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-04-17 16:13:36 +0500

    get api of projects

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const dev = require("./dev_information");
+const projects = require("./projects");
 
 router.use("/dev_information", dev);
+router.use("/projects", projects); 
 
 module.exports = router;
