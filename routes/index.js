246755bc7a74b5910bf561276c69b94ffb5dd53f
Shuja-Siddiqui <shuja0094@gmail.com>
2023-04-17 16:21:09 +0500

    testimonials get request

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const dev = require("./dev_information");
 const service = require("./services");
+const testimonials = require("./testimonials");
 
 router.use("/dev_information", dev);
 router.use("/services", service);
+router.use("/testimonials", testimonials);
 
 module.exports = router;
