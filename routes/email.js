c1ac8534a9b896dc34e3959bb033b3ed5a76b3d5
Abdul Raheem <shuja0094@gmail.com>
2023-06-12 21:18:57 +0500

    Update email.js

--- routes/email.js
+++ routes/email.js
@@ -2,7 +2,6 @@ const route = require("express").Router();
 const nodemailer = require("nodemailer");
 
 route.post("/", async (req, res) => {
-  console.log("req body", req.body);
   try {
     const { name, email, message } = req.body;
     const transporter = nodemailer.createTransport({
@@ -17,12 +16,11 @@ route.post("/", async (req, res) => {
 
     const info = await transporter.sendMail({
       from: email,
-      to: "umarwork1663@gmail.com",
+      to: "abdulraheem@consoledot.com",
       subject: `New message from ${name}`,
       text: message,
     });
 
-    console.log("Email sent:", info.messageId);
     res.status(200).send("Email sent");
   } catch (error) {
     console.error("Error sending email:", error);
