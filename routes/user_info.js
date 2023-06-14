d315b7e65c538754b0a4ac643ae6bc0da8e769cf
Abdul Raheem <shuja0094@gmail.com>
2023-06-14 11:47:21 +0500

    backend

--- routes/user_info.js
+++ routes/user_info.js
@@ -15,15 +15,25 @@ route.get("/:id", auth, async (req, res) => {
 route.patch("/:id", auth, async (req, res) => {
   const { new_username, username, password, new_password } = req.body;
   try {
-    const user = await UserInfo.findByIdAndUpdate(
-      { _id: req.params.id },
-      {
-        new_username,
-        new_password,
-      },
-      { new: true }
-    );
-    return setResponse(res, "Updated", user, 200);
+    const user = await UserInfo.findById({ _id: req.params.id });
+    if (username && user.username !== username) {
+      return setResponse(res, "Not found", null, 404);
+    }
+    if (password && user.password === password) {
+      return setResponse(res, "Not found", null, 404);
+    }
+
+    user.username = username;
+    user.password = password;
+    // const updatedUser = await UserInfo.findByIdAndUpdate(
+    //   { _id: req.params.id },
+    //   {
+    //     new_username,
+    //     new_password,
+    //   },
+    //   { new: true }
+    // );
+    return setResponse(res, "Updated", updatedUser, 200);
   } catch (error) {
     return setResponse(res, "Internal Server Error", null, 500);
   }
@@ -31,16 +41,6 @@ route.patch("/:id", auth, async (req, res) => {
 
 route.get("/login", async (req, res) => {
   res.send("Logging in");
-  // try {
-  //   const { username, password } = req.body;
-  //   const users = await UserInfo.findOne({ username, password });
-  //   if (!users) {
-  //     return setResponse(res, "username/password incorrect", null, 404);
-  //   }
-  //   return setResponse(res, "Login Successfull", users, 200);
-  // } catch {
-  //   return setResponse(res, "Internal Server Error", null, 500);
-  // }
 });
 
 module.exports = route;
