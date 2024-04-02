3bf3cc7a078190234e055de0219f914032ea986d
Shuja-Siddiqui <shuja0094@gmail.com>
2024-04-02 16:52:25 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
-const path = require("node:path");
-const file = require("express-fileupload");
-require("dotenv").config({ path: path.join(__dirname, "../.env") });
-
+require("dotenv").config();
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const db = require("./db");
+const path = require("node:path");
+app.use(express.json());
+app.use(cors('*'));
 const { router } = require("./routes");
+const file = require("express-fileupload");
+
 const PORT = process.env.PORT || 5000;
 
-const app = express();
 
 // Middlewares
 
-app.use(express.json());
-app.use(cors('*'));
-app.use(express.static(path.join(__dirname, "../public")));
 app.use(file());
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Routes
 
-app.use((req, res, next) => {
-  console.log("Incoming request:", req.method, req.url);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Incoming request:", req.method, req.url);
+//   next();
+// });
 
 // Mounting router
 app.use("/api/v1", router);
